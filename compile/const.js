a707d3c98208998993f68f86b60a551db65f4753
Raymond Li <it1011@qq.com>
2017-12-02 17:37:18 +0800

    fix(build): 修改生产和开发环境的构建流程

--- compile/const.js
+++ compile/const.js
@@ -2,5 +2,6 @@ module.exports = {
     srcTemplte: './src/index.html',
     targetTemplate: './dist/index.html',
     cssReg: /{{css}}/,
-    jsReg: /{{js}}/
+    jsReg: /{{js}}/,
+    prod: "production"
 }
--- compile/index.js
+++ compile/index.js
@@ -5,18 +5,23 @@ const constants = require('./const');
 const webpackConfig = require('../webpack.config');
 const compiler = webpack(webpackConfig);
 const cwd = process.cwd();
+const contants = require('./const');
 
-compiler.watch({},(err, {compilation}) => {
+function buildCallback(err, {compilation}){
     if(err) return  console.log(err);
     let {assets,entrypoints: {main: {chunks}}} = compilation;
     let cssFiles = require('./get-css')(assets);
     let jsFiles = require('./get-js')(chunks)
     if(fs.existsSync(path.resolve(cwd, constants.targetTemplate))) return;
     require('./write-index-page')(cssFiles, jsFiles);
-});
+}
 
-process.on('beforeExit', () => {
-    compiler.close(function(){
-
+if(process.env.NODE_ENV===constants.prod){
+    compiler.run(buildCallback);
+}
+else{
+    compiler.watch({}, buildCallback);
+    process.on('beforeExit', () => {
+        compiler.close();
     });
-});
+}
